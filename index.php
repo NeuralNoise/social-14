977bb96f0d5c9a78ffa8d6da3edcafb60c53c88e
Mmm-bacon <nnorris25@gmail.com>
2016-06-26 22:09:54 -0700

    initial commit. starting on the display, using bootstrap template.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Shawn Norris
+ * @copyright 2016
+ * This copyright notice must remain in the header section of this file.
+ */
+
+//include core file to gain access to classes and functions.
+include 'core.php';
+
+?>
