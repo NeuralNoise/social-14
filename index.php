81c7e2322763f7d039f8322746522d381ea7f459
Mmm-bacon <nnorris25@gmail.com>
2016-07-05 19:05:37 -0700

    page display function

--- index.php
+++ index.php
@@ -8,4 +8,27 @@
 //include core file to gain access to classes and functions.
 include 'core.php';
 
+//display header
+
+page_display('header');
+
+//display nav bar
+
+page_display('pre_content');
+
+?>
+
+<!-- content goes here --->
+<h1>The Live Feed - What's Happening?</h1>
+
+<?php
+
+//display post content to shore things up
+
+page_display("post_content");
+
+//display footer
+
+page_display("footer");
+
 ?>
